b1f76340b2a249739e14a6974e2b8699d804ae3b
Vinicius Bastos <vclemente2@hotmail.com>
2023-10-10 18:51:35 -0300

    fix: correção nas validações dos dados de usuário.

--- src/main/java/com/generation/blogpessoal/dto/user/CreateUserDto.java
+++ src/main/java/com/generation/blogpessoal/dto/user/CreateUserDto.java
@@ -1,7 +1,6 @@
 package com.generation.blogpessoal.dto.user;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -12,14 +11,14 @@ public record CreateUserDto(
 
         @Email(message = "The field email must be in a valid email format.")
         @NotBlank(message = "The email field is required.")
-        @Max(value = 255, message = "The email field must be shorter than 255 characters.")
+        @Size(max = 255, message = "The email field must be shorter than 255 characters.")
         String email,
 
         @NotBlank(message = "The password field is required.")
         @Size(min = 5, max = 30, message = "The password must be between 3 and 255 characters long.")
         String password,
 
-        @Max(value = 5000, message = "The image field must be shorter than 5000 characters.")
+        @Size(max = 5000, message = "The image field must be shorter than 5000 characters.")
         String image
 ) {
 }
--- src/main/java/com/generation/blogpessoal/dto/user/UpdateUserDto.java
+++ src/main/java/com/generation/blogpessoal/dto/user/UpdateUserDto.java
@@ -1,6 +1,6 @@
 package com.generation.blogpessoal.dto.user;
 
-import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -11,13 +11,14 @@ public record UpdateUserDto(
         @Size(min = 3, max = 255, message = "The name must be between 3 and 255 characters long.")
         String name,
 
-        @Max(value = 255, message = "The email field must be shorter than 255 characters.")
+        @Email(message = "The field email must be in a valid email format.")
+        @Size(max = 255, message = "The email field must be shorter than 255 characters.")
         String email,
 
         @Size(min = 5, max = 30, message = "The password must be between 3 and 255 characters long.")
         String password,
 
-        @Max(value = 5000, message = "The image field must be shorter than 5000 characters.")
+        @Size(max = 5000, message = "The image field must be shorter than 5000 characters.")
         String image
 ) {
 }
