eed546950bf35465d9f691d411dbc025c4d760ce
Vinicius Bastos <vclemente2@hotmail.com>
2023-10-13 12:31:14 -0300

    Refatoração da URI dos enpoints de usuário e renomeação dos Dtos de security.

--- src/main/java/com/generation/blogpessoal/controller/UserController.java
+++ src/main/java/com/generation/blogpessoal/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/usuarios")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
 
